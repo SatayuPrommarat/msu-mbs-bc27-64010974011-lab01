4dad2980eb641ea77deeca4f96134533894f1ef1
SatayuPrommarat <108518654+SatayuPrommarat@users.noreply.github.com>
2022-07-08 15:26:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
    <?php echo '<p>My ID : 64010974011</p>'; ?> 
-     <?php echo '<p>Update-1st</p>'; ?>       
+     <?php echo '<p>Update-1st</p>'; ?> 
+     <?php echo '<p>Update-2st</p>'; ?> 
  </body>
 </html>
