7f2a0578b99e6a5c0ec684c67667695fec179c9c
SatayuPrommarat <108518654+SatayuPrommarat@users.noreply.github.com>
2022-07-08 15:23:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>My ID : 64010974011</p>'; ?> 
+     <?php echo '<p>Update-1st</p>'; ?>       
+ </body>
+</html>
